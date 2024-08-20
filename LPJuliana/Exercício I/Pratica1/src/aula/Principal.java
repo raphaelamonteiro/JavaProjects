147e57bfea0f163a2c429523b190f2a1f6dfab6b
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2024-08-19 21:48:27 -0300

    Add files via upload

---
+++ LPJuliana/Exercício I/Pratica1/src/aula/Principal.java
@@ -0,0 +1,11 @@
+package aula;
+
+public class Principal {
+    public static void main (String[] args){
+        int x = 1;
+        do{
+            System.out.println(x);
+            x++;
+        } while(x<6);
+    }
+}
