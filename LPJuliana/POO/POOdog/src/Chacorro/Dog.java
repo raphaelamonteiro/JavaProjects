8c6395d604c1f6aacd50a01ad4151bb2cfc76c3e
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2024-08-21 08:17:36 -0300

    Add files via upload

---
+++ LPJuliana/POO/POOdog/src/Chacorro/Dog.java
@@ -0,0 +1,15 @@
+package Chacorro;
+
+class Dog {
+    String nome;
+    int peso;
+
+    void latir(){
+        if (peso>= 9){
+            System.out.println("AUUUUUU!");
+        }else if (peso < 9){
+            System.out.println("AU!");
+        }
+
+    }
+}
---
+++ LPJuliana/POO/POOdog/src/Chacorro/Teste.java
@@ -0,0 +1,16 @@
+package Chacorro;
+
+public class Teste {
+    public static void main(String[]args){
+        Dog primeiro=new Dog();
+        primeiro.nome="Nina";
+        primeiro.peso=1;
+
+        Dog segundo=new Dog();
+        segundo.nome="Bravo";
+        segundo.peso=10;
+
+        primeiro.latir();
+        segundo.latir();
+    }
+}
