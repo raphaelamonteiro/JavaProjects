29ea5f8479dc3955aadff0f495327a153f1e3613
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2024-08-21 08:46:15 -0300

    Add files via upload

---
+++ LPJuliana/Pratica1/Exercício 5/Pratica1/src/aula/Principal.java
@@ -0,0 +1,11 @@
+package aula;
+
+public class Principal {
+    public static void main (String[] args){
+        int y = 0;
+        for(int x = 0; x < 11; x++){
+            y = y+x;
+        }
+        System.out.println(y);
+    }
+}
