a193138cf553b7890ace6bd6d627af4750ef2264
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2024-08-21 08:31:52 -0300

    Add files via upload

---
+++ LPJuliana/Pratica1/Exercício 4/Pratica1/src/aula/Principal.java
@@ -0,0 +1,12 @@
+package aula;
+
+public class Principal {
+    public static void main (String[] args){
+        for(int x = 1; x < 16; x++){
+            if(x % 2 == 0){
+                System.out.println(x);
+            }
+        }
+
+    }
+}
