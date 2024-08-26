d34e4ee2fbd24ca6c0c6aa71f2abacb6574b1c55
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2024-08-26 10:14:07 -0300

    Add files via upload

---
+++ LPJuliana/Pratica1/Exercicio 7/Pratica1/src/aula/Principal.java
@@ -0,0 +1,8 @@
+package aula;
+import javax.swing.JOptionPane;
+public class Principal {
+    public static void main(String[] args){
+        String nome = JOptionPane.showInputDialog("Entre com o nome: ");
+
+    }
+}
