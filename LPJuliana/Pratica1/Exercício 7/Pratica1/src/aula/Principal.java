e9d77e5c8219c0d6b95934ecae574994258a5dbc
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2024-08-26 10:18:05 -0300

    Update Principal.java

--- LPJuliana/Pratica1/Exercício 7/Pratica1/src/aula/Principal.java
+++ LPJuliana/Pratica1/Exercício 7/Pratica1/src/aula/Principal.java
@@ -2,7 +2,10 @@ package aula;
 import javax.swing.JOptionPane;
 public class Principal {
     public static void main(String[] args){
-        String nome = JOptionPane.showInputDialog("Entre com o nome: ");
-
+        String nome = JOptionPane.showInputDialog("Entre com um nome: ");
+        if (nome != null && !nome.equals("")){
+            System.out.println(nome);
+        }
     }
+
 }
