d953eeb83387a8b4337d2637ddab3ffbff443e10
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2024-08-26 10:19:20 -0300

    Update Principal.java

--- LPJuliana/Pratica1/Exercício 8/Pratica1/src/aula/Principal.java
+++ LPJuliana/Pratica1/Exercício 8/Pratica1/src/aula/Principal.java
@@ -2,7 +2,13 @@ package aula;
 import javax.swing.JOptionPane;
 public class Principal {
     public static void main(String[] args){
-        String nome = JOptionPane.showInputDialog("Entre com o nome: ");
-
+        String valor = JOptionPane.showInputDialog("Entre com um número: ");
+        int numero = Integer.parseInt(valor);
+        if (numero % 2 == 0) {
+           JOptionPane.showMessageDialog(null, "O número "+ numero+ " é par");
+        } else{
+            JOptionPane.showMessageDialog(null, "O número "+ numero+ " é ímpar");
+        }
     }
+
 }
